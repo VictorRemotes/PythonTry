66975d95455558c93b10eeac22c3361ad9a02d9c
TAKO <victor.chen@remotes.com.tw>
2022-04-26 16:43:00 +0800

    2

--- DownloadInformation.py
+++ DownloadInformation.py
@@ -1,15 +1,19 @@
 import datetime
+from email import message
+import imp
+import os
 
 import chardet
 import loguru
 import pyquery
 import requests
+from Propotion import Propotion
 
 
 def main():
 
     resp = requests.get('https://www.taifex.com.tw/cht/9/futuresQADetail')
-    
+
     if resp.status_code != 200:
         loguru.logger.error('RESP: status code is nnot 200')
 
@@ -26,12 +30,65 @@ def main():
                 txt = resp.content.decode('utf-8')
     except Exception as e:
         loguru.logger.error(e)
-        
+
     if txt is None:
         return
-    
+
     loguru.logger.info(txt)
-    
+    # 成分股市值佔比清單
+    proportions = []
+
+    # 將下載回來的內容解析為 PyQuery 物件
+    d = pyquery.PyQuery(txt)
+
+    # 透過 CSS 選擇器取出所有表格行
+    trs = list(d('table tr').items())
+
+    # 去除標頭行（分析結果 1.）
+    trs = trs[1:]
+
+    # 依序取出資料行
+    for tr in trs:
+
+        tds = list(tr('td').items())
+
+        code = tds[1].text().strip()
+
+        if code != '':
+            sort = tds[1].text().strip()
+            name = tds[2].text().strip()
+            percent = tds[3].text().strip()
+
+            proportions.append(Propotion(
+                sort=sort,
+                code=code,
+                name=name,
+                percent=percent
+            ))
+
+        code = tds[5].text().strip()
+
+        if code != '':
+
+            sort = tds[5].text().strip()
+
+            name = tds[6].text().strip()
+
+            percent = tds[7].text().strip()
+
+            proportions.append(Propotion(
+                sort=sort,
+                code=code,
+                name=name,
+                percent=percent
+            ))
+
+        proportions.sort(key=lambda proportion: proportion.Code)
+        loguru.logger.info(proportions)
+
+        message = os.linesep.join([str(proportion)
+                                  for proportion in proportions])
+        loguru.logger.info('PROPORTIONS' + os.linesep + message)
 
     if __name__ == '_main_':
         loguru.logger.add(
--- Propotion.py
+++ Propotion.py
@@ -1,12 +1,20 @@
+import fractions
+
+
 class Propotion:
-    def __init__(self,sort,code,name,percent):
-        
+
+    def __init__(self, sort, code, name, percent):
         self.Sort = sort
-        
         self.Code = code
-        
         self.Name = name
-        
-        self.Percent = percent
-        
-        
+        self.Percent = fractions.Fraction(percent[:-1])
+
+    def __repr__(self):
+        return(
+            f'class Propotion {{'
+            f'Sort={self.Sort}'
+            f'Code={self.Code}'
+            f'Name={self.Name}'
+            f'Percent={float(self.Percent):.4f}%'
+            f'}}'
+        )
